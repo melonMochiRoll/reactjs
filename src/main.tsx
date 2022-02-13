a096f81ae4efec410f305792886764c043924277
melonMochiRoll <soulhwasin@gmail.com>
2022-02-13 23:42:12 +0900

    Add axios defaults settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import LogIn from './pages/LogIn';
+import Join from './pages/Join';
 
-render(<div>hello, react</div>, document.querySelector('#app'));
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:3005";
+
+render(
+<BrowserRouter>
+  <Routes>
+    <Route path="/login" element={<LogIn />}/>
+    <Route path="/join" element={<Join />}/>
+  </Routes>
+</BrowserRouter>,
+document.querySelector('#app'));

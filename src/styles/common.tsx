19fddd3affc1d663fc0b7cdd785ac5c94385a03e
melonMochiRoll <soulhwasin@gmail.com>
2022-02-13 23:36:36 +0900

    Add common styled

---
+++ src/styles/common.tsx
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 30px;
+`;
+
+export const Input = styled.input`
+  margin: 5px;
+  padding: 10px;
+  font-size: 18px;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  margin: 5px;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 18px;
+  border: none;
+  border-radius: 14px;
+  background-color: #005aff;
+  color: #fff;
+
+  &:hover {
+    background-color: #2471ff;
+  }
+`;

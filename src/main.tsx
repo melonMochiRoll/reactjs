ecff4e253842a8dcffdf68323e43ae043f4a1d9e
melonMochiRoll <soulhwasin@gmail.com>
2022-02-19 18:30:08 +0900

    Add NotFound page

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
-import LogIn from './pages/LogIn';
-import Join from './pages/Join';
+import LogIn from '@Pages/LogIn';
+import Join from '@Pages/Join';
+import NotFound from '@Pages/NotFound';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:3005";
@@ -13,6 +14,7 @@ render(
   <Routes>
     <Route path="/login" element={<LogIn />}/>
     <Route path="/join" element={<Join />}/>
+    <Route path="/*" element={<NotFound />}/>
   </Routes>
 </BrowserRouter>,
 document.querySelector('#app'));

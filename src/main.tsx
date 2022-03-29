81908547fe6f29fef5c3d587666e9b96f4f9823b
melonMochiRoll <soulhwasin@gmail.com>
2022-03-30 01:20:26 +0900

    Fix axios config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import axios from 'axios';
 import LogIn from '@Pages/LogIn';
 import Join from '@Pages/Join';
 import NotFound from '@Pages/NotFound';
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:3005";
 
 render(
 <BrowserRouter>

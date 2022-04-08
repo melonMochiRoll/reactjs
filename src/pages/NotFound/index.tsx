27c74f7313e5c6a25b9b107895c8607e226adbe1
melonMochiRoll <soulhwasin@gmail.com>
2022-04-08 21:45:09 +0900

    Fix NotFound page button

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,14 +1,17 @@
-import { Button, Container } from '@Src/styles/common';
+import { Button } from '@mui/material';
+import { Container } from '@Src/styles/common';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <Container>
-      <div>존재하지 않는 URL입니다.</div><br/>
-      <Link to={'/'}>
-        <Button>홈으로</Button>
-      </Link>
+      <div>존재하지 않는 URL입니다.</div>
+      <Button
+        variant="contained"
+        component={Link}
+        to={'/login'}
+        sx={{ mt: 3 }}>홈으로</Button>
     </Container>
   )
 }

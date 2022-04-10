a3034e0d71f53a31567556fe8e6599a2e92d3b0d
melonMochiRoll <soulhwasin@gmail.com>
2022-04-11 03:08:19 +0900

    Rename instance name

--- src/utils/axiosInstance/index.ts
+++ src/utils/axiosInstance/index.ts
@@ -1,9 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
-const instance: AxiosInstance = axios.create({
+export const axiosClient: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3005',
   withCredentials: true,
   timeout: 1000,
 });
-
-export default instance;

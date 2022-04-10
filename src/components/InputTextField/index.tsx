ce25c3d817a9250f93d898f5d462dc9964bb0181
melonMochiRoll <soulhwasin@gmail.com>
2022-04-11 03:09:01 +0900

    Add Props errorMessage

--- src/components/InputTextField/index.tsx
+++ src/components/InputTextField/index.tsx
@@ -1,4 +1,4 @@
-import { FormControl, Input, InputLabel } from '@mui/material';
+import { FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 import React, { FC } from 'react';
 
 interface Props {
@@ -8,9 +8,18 @@ interface Props {
   value: string,
   onChange: React.ChangeEventHandler,
   onKeyUp?: any,
+  errorMessage?: string,
 }
 
-export const InputTextField: FC<Props> = ({ type = 'text', label, name, value, onChange, onKeyUp }) => {
+export const InputTextField: FC<Props> = ({
+  type = 'text',
+  label,
+  name,
+  value,
+  onChange,
+  onKeyUp,
+  errorMessage,
+  }) => {
   return (
     <FormControl
       variant="standard"
@@ -23,6 +32,7 @@ export const InputTextField: FC<Props> = ({ type = 'text', label, name, value, o
         onChange={onChange}
         onKeyUp={onKeyUp}
       />
+      <FormHelperText>{errorMessage}</FormHelperText>
     </FormControl>
   )
 };

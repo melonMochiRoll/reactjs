8a28556316f65d54688f874bbc137699a4913868
melonMochiRoll <soulhwasin@gmail.com>
2022-04-28 19:15:51 +0900

    Fix onSubmit

--- src/pages/Join/JoinContainer.tsx
+++ src/pages/Join/JoinContainer.tsx
@@ -105,10 +105,11 @@ const JoinContainer: FC = () => {
     try {
       await onJoin(joinSubmitForm);
       toast.success('회원가입이 완료되었습니다.');
-      navigate('/login');
     } catch(e) {
       console.error(e);
       toast.error('회원가입에 실패했습니다.');
+    } finally {
+      navigate('/login');
     }
   };
 

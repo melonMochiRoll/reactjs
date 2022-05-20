7a7810836a4715adb3137ebd10d2e98bb9ac3209
melonMochiRoll <soulhwasin@gmail.com>
2022-05-20 16:32:59 +0900

    Rename HomeLayout

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,24 +1,13 @@
-import { Box } from "@mui/material";
 import React, { FC } from "react";
-import { MainBackground } from "@Src/components/MainLayout";
-import Header from "@Src/components/Header";
+import Header from "@Components/Header";
+import MainLayout from "@Components/MainLayout";
+import { MainBackground } from "@Styles/common";
 
 const MainPage: FC = () => {
   return (
     <MainBackground>
       <Header />
-
-      <Box sx={{
-        display: 'flex',
-        maxWidth: 700,
-        height: 500,
-        marginX: 'auto',
-        marginY: 0,
-        mt: 3,
-        backgroundColor: '#fffcf9',
-        }}>
-          MainBox
-      </Box>
+      <MainLayout />
     </MainBackground>
   )
 }

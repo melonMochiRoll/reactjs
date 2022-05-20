0dd379efcceff10f19e32cb8d1bd149275da0b97
melonMochiRoll <soulhwasin@gmail.com>
2022-05-20 15:54:23 +0900

    Delete container styled

--- src/styles/common.tsx
+++ src/styles/common.tsx
@@ -1,10 +1,5 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-  margin: 0 auto;
-  padding: 30px;
-`;
-
 export const Form = styled.form`
   display: flex;
   flex-direction: column;

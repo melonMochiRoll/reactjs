0339344d53459b95340d6225022b1092d717d893
melonMochiRoll <soulhwasin@gmail.com>
2022-05-20 16:33:44 +0900

    Add MainBackground

--- src/styles/common.tsx
+++ src/styles/common.tsx
@@ -1,5 +1,15 @@
 import styled from "@emotion/styled";
 
+export const MainBackground = styled.div`
+  position: fixed;
+  text-align: center;
+  left: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  background-color: #e8e8e8;
+`;
+
 export const Form = styled.form`
   display: flex;
   flex-direction: column;

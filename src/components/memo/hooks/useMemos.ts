6d142d380ea9ef3a53fcdf8c1c14f6db3014c74b
melonMochiRoll <soulhwasin@gmail.com>
2022-06-10 21:55:41 +0900

    Replace path

--- src/components/memo/hooks/useMemos.ts
+++ src/components/memo/hooks/useMemos.ts
@@ -3,19 +3,19 @@ import { axiosClient } from "@Utils/axiosInstance";
 import { useQuery } from "react-query";
 
 const useMemos = ( userId: number, folderName: string ) => {
-  const { isFetching: memoLoading, data: memoData, refetch: memoRefetch } = useQuery<Memo[], Error, Memo[], [string, number, string]>({
+  const { isFetching: loading, data, refetch } = useQuery<Memo[], Error, Memo[], [string, number, string]>({
     queryKey: ['MyMemoData', userId, folderName],
     queryFn: getMemos,
     enabled: false,
     retry: false,
   });
 
-  return { memoLoading, memoData, memoRefetch };
+  return { loading, data, refetch };
 };
 
 export default useMemos;
 
 const getMemos = async ({ queryKey }: { queryKey: [string, number, string] }): Promise<Memo[]> => {
-  const { data } = await axiosClient({ url: `api/memo?id=${queryKey[1]}&fn=${queryKey[2]}` });
+  const { data } = await axiosClient({ url: `api/memo?id=${queryKey[1] | 0}&fn=${queryKey[2]}` });
   return data;
 };

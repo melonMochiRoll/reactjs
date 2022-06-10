4ad2d9c3d23b87379acb5b92a4279349b47ce48a
melonMochiRoll <soulhwasin@gmail.com>
2022-06-10 17:08:33 +0900

    Replace useMemoCount

--- src/components/folder/hooks/useMemoCount.ts
+++ src/components/folder/hooks/useMemoCount.ts
@@ -4,22 +4,22 @@ import { useQuery } from "react-query";
 export interface MemoCount {
   folderName: string,
   count: number,
-};
+}
 
 const useMemoCount = (userId: number) => {
-  const { isFetching: memoCountloading, data: memoCount, refetch: memoCountRefetch } = useQuery<MemoCount[], Error, MemoCount[], [string, number]>({
+  const { isFetching: loading, data, refetch } = useQuery<MemoCount[], Error, MemoCount[], [string, number]>({
     queryKey: ['MyMemoCount', userId],
     queryFn: getMemoCount,
     enabled: false,
     retry: false,
   });
 
-  return { memoCountloading, memoCount, memoCountRefetch };
+  return { loading, data, refetch };
 };
 
 export default useMemoCount;
 
 const getMemoCount = async ({ queryKey }: { queryKey: [string, number] } ) => {
-  const { data } = await axiosClient({ url: `api/memo/count?id=${queryKey[1]}`});
+  const { data } = await axiosClient({ url: `api/memo/count?id=${queryKey[1] | 0}`});
   return data;
 };

7fa6e5192a93e4b330a06e1e4e02692e055dfe8c
melonMochiRoll <soulhwasin@gmail.com>
2022-06-16 13:46:04 +0900

    Add interface MemoWithHasMore

--- src/typings/model.ts
+++ src/typings/model.ts
@@ -1,22 +1,27 @@
 export interface User {
-  id: number,
-  email: string,
-  nickname: string,
+  id: number;
+  email: string;
+  nickname: string;
 }
 
 export interface Memo {
-  id: number,
-  createdAt: string,
-  updatedAt: string,
-  publicMode: boolean,
-  contents: string,
-  folderName: string,
-  userId: number,
-  tags: Tag[],
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publicMode: boolean;
+  contents: string;
+  folderName: string;
+  userId: number;
+  tags: Tag[];
+}
+
+export interface MemoWithHasMore {
+  memos: Memo[];
+  hasMore: boolean;
 }
 
 export interface Tag {
-  id: number,
-  tag: string,
-  createdAt: string,
+  id: number;
+  tag: string;
+  createdAt: string;
 }
